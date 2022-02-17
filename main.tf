9ceb6837aed281e5575fb352b634c984456d05c8
AKSHAY ASHOK CHANDANSHIVE <49795892+chandanshive@users.noreply.github.com>
2022-02-17 17:04:36 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,21 @@
+# Auto-remediate
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_instance" "webserver" {
+  ami           = "ami-01e36b7901e884a10"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "webserver_disney"
+  }
+}
+
+resource "aws_s3_bucket" "accuricsbucketdemo" {
+  bucket = "my-tf-test-bucket"
+
+  tags = {
+    Name = "bucketdemo_disney"
+  }
+}
